c889e2f19f67e1708037209ea08cb42f18f33766
ovi-khan <manikkhan2240@gmail.com>
2023-05-23 09:59:26 +0600

    readme.md file add

---
+++ name-of-your-project/README.md
@@ -0,0 +1,14 @@
+# Car Toy Market 
+# I have a mistake create private site repo link please check 'Name-of-your-project this file'
+# I can not build full project but i tryed to my best
+
+## [Live website deploy Link](https://toy-market-809e7.web.app)
+
+
+## [Client Side  Repo Link](https://github.com/programming-hero-web-course-4/b7a11-toy-marketplace-client-side-ovi-khan)
+Click here for the  repo: [https://github.com/programming-hero-web-course-4/b7a11-toy-marketplace-client-side-ovi-khan](https://github.com/programming-hero-web-course-4/b7a11-toy-marketplace-client-side-ovi-khan)
+
+
+
+## [Server Side Repo Link](https://github.com/programming-hero-web-course-4/b7a11-toy-marketplace-server-side-ovi-khan)
+Click here for the private repo: [https://github.com/programming-hero-web-course-4/b7a11-toy-marketplace-server-side-ovi-khan](https://github.com/programming-hero-web-course-4/b7a11-toy-marketplace-server-side-ovi-khan)
--- name-of-your-project/src/Pages/Status/Status.jsx
+++ name-of-your-project/src/Pages/Status/Status.jsx
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import CountUp from 'react-countup';
 
 const Status = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount(prevCount => prevCount + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   return (
     <div>
-      <h1 className="text-center text-5xl font-bold mb-20">
+      <h1 className="text-center text-5xl font-bold mb-10">
         Our Current Client
       </h1>
       <div className="lg:flex  gap-5 shadow mb-20 p-5">
-        <div className="stat place-items-center bg-blue-400 rounded-lg mt-2">
+        <div className="stat place-items-center bg-blue-400 rounded-lg mt-2 ">
           <div className="stat-title text-black">Downloads</div>
-          <div className="stat-value text-yellow-50">31K</div>
+          <div className="stat-value text-yellow-50">{count}K</div>
           <div className="stat-desc text-black">From January 1st to February 1st</div>
         </div>
 
         <div className="stat place-items-center bg-orange-400 rounded-lg mt-2">
           <div className="stat-title text-black">Users</div>
-          <div className="stat-value text-secondary">4,200</div>
+          <div className="stat-value text-secondary"><CountUp end={5000} />+</div>
           <div className="stat-desc text-secondary text-black">↗︎ 40 (2%)</div>
         </div>
 
         <div className="stat place-items-center bg-cyan-300 rounded-lg mt-2">
           <div className="stat-title text-black">New Registers</div>
-          <div className="stat-value">1,200</div>
+          <div className="stat-value"><CountUp end={1200} />+</div>
           <div className="stat-desc text-black">↘︎ 90 (14%)</div>
         </div>
       </div>
